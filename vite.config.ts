eb0c790c182e2628e0a7960871ac50ecbf6b11e6
borisaxel <boris.ebaneth@ossas-it.com>
2025-12-22 21:31:54 +0100

    feat: Install icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,31 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueDevTools()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // On dit à Vue de considérer toutes les balises commençant par 'si-'
+          // comme des Custom Elements natifs
+          isCustomElement: (tag) => tag.startsWith('si-'),
+        },
+      },
+    }),
+    tailwindcss(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/@meindonsa/space-icons/dist/space-icons.svg',
+          dest: './',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
