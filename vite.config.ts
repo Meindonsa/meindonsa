eea831e6c0ab61f310c4cdaee7dbb6bf2bfb5887
Meindonsa <58621389+Meindonsa@users.noreply.github.com>
2025-12-23 01:15:38 +0100

    Simplify Vite configuration by removing custom element options

--- vite.config.ts
+++ vite.config.ts
@@ -9,25 +9,9 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          // On dit à Vue de considérer toutes les balises commençant par 'si-'
-          // comme des Custom Elements natifs
-          isCustomElement: (tag) => tag.startsWith('si-'),
-        },
-      },
-    }),
+    vue(),
     tailwindcss(),
-    vueDevTools(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/@meindonsa/space-icons/dist/space-icons.svg',
-          dest: './',
-        },
-      ],
-    }),
+    vueDevTools()
   ],
   resolve: {
     alias: {
