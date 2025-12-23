00375a855471d6e3592fd4ac6e2a7eaef135f582
borisaxel <boris.ebaneth@ossas-it.com>
2025-12-23 14:41:29 +0100

    feat: Add filter for skills

--- src/components/skill/SkillItem.vue
+++ src/components/skill/SkillItem.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, onMounted, onUnmounted, ref } from 'vue'
 
 const { data } = defineProps(['data'])
+const isMobile = ref(false)
 const show = ref(data.show)
+
+const checkMobile = () => {
+  isMobile.value = window.innerWidth <= 768
+}
 
 const toggle = () => {
   show.value = !show.value
 }
 
+onMounted(() => {
+  checkMobile()
+  window.addEventListener('resize', checkMobile)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', checkMobile)
+})
 </script>
 
 <template>
   <div
-    class="border border-gray-700 py-3 px-3 rounded-sm bg-center bg-no-repeat bg-cover"
+    class="border border-gray-700 py-5 px-3 rounded-sm bg-center bg-no-repeat bg-cover cursor-pointer"
     :style="[
       show
         ? {
@@ -20,17 +33,29 @@ const toggle = () => {
           }
         : {},
     ]"
-    @mouseenter="show = true"
-    @mouseleave="show = false"
+    @mouseenter="isMobile ? null : (show = true)"
+    @mouseleave="isMobile ? null : (show = false)"
   >
     <h1 @click="toggle" class="text-2xl font-semibold" :style="{ color: data.color }">
       {{ data.name }}
     </h1>
-    <div v-show="show">
-      <h2 class="text-gray-50">{{ data.category }}</h2>
-      <p class="text-gray-500">{{ data.description }}</p>
-    </div>
+    <Transition mode="in-out">
+      <div v-show="show">
+        <h2 class="text-gray-50">{{ data.category.label }}</h2>
+        <p class="text-gray-500">{{ data.description }}</p>
+      </div>
+    </Transition>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
